9bddfd42acb1e22b84618a7e7043e334509feea1
U=Cormorant <u@chimata.org>
2014-01-30 03:58:59 +0900

    added README.md; appended the result to clone_and_deepclone.pl

--- clone_and_deepclone.pl
+++ clone_and_deepclone.pl
@@ -146,3 +146,55 @@ cmpthese(100000 => +{
 }, 'auto');
 
 exit;
+
+__END__
+Benchmark: timing 100000 iterations of nest_1_clone, nest_1_dclone...
+nest_1_clone: 1.40845 wallclock secs ( 1.41 usr +  0.00 sys =  1.41 CPU) @ 70921.99/s (n=100000)
+nest_1_dclone: 2.62968 wallclock secs ( 2.62 usr +  0.00 sys =  2.62 CPU) @ 38167.94/s (n=100000)
+                 Rate nest_1_dclone  nest_1_clone
+nest_1_dclone 38168/s            --          -46%
+nest_1_clone  70922/s           86%            --
+
+Benchmark: timing 100000 iterations of nest_2_clone, nest_2_dclone...
+nest_2_clone: 2.49977 wallclock secs ( 2.50 usr +  0.00 sys =  2.50 CPU) @ 40000.00/s (n=100000)
+nest_2_dclone: 3.47202 wallclock secs ( 3.47 usr +  0.00 sys =  3.47 CPU) @ 28818.44/s (n=100000)
+                 Rate nest_2_dclone  nest_2_clone
+nest_2_dclone 28818/s            --          -28%
+nest_2_clone  40000/s           39%            --
+
+Benchmark: timing 100000 iterations of nest_3_clone, nest_3_dclone...
+nest_3_clone: 3.45251 wallclock secs ( 3.45 usr +  0.00 sys =  3.45 CPU) @ 28985.51/s (n=100000)
+nest_3_dclone: 4.39019 wallclock secs ( 4.38 usr +  0.00 sys =  4.38 CPU) @ 22831.05/s (n=100000)
+                 Rate nest_3_dclone  nest_3_clone
+nest_3_dclone 22831/s            --          -21%
+nest_3_clone  28986/s           27%            --
+
+Benchmark: timing 100000 iterations of nest_4_clone, nest_4_dclone...
+nest_4_clone: 4.56922 wallclock secs ( 4.56 usr +  0.00 sys =  4.56 CPU) @ 21929.82/s (n=100000)
+nest_4_dclone: 5.1285 wallclock secs ( 5.11 usr +  0.01 sys =  5.12 CPU) @ 19531.25/s (n=100000)
+                 Rate nest_4_dclone  nest_4_clone
+nest_4_dclone 19531/s            --          -11%
+nest_4_clone  21930/s           12%            --
+
+Benchmark: timing 100000 iterations of nest_5_clone, nest_5_dclone...
+nest_5_clone: 5.5243 wallclock secs ( 5.52 usr +  0.00 sys =  5.52 CPU) @ 18115.94/s (n=100000)
+nest_5_dclone: 5.89666 wallclock secs ( 5.89 usr +  0.00 sys =  5.89 CPU) @ 16977.93/s (n=100000)
+                 Rate nest_5_dclone  nest_5_clone
+nest_5_dclone 16978/s            --           -6%
+nest_5_clone  18116/s            7%            --
+
+Benchmark: timing 100000 iterations of nest_6_clone, nest_6_dclone...
+nest_6_clone: 6.47901 wallclock secs ( 6.47 usr +  0.00 sys =  6.47 CPU) @ 15455.95/s (n=100000)
+nest_6_dclone: 6.69936 wallclock secs ( 6.66 usr +  0.01 sys =  6.67 CPU) @ 14992.50/s (n=100000)
+                 Rate nest_6_dclone  nest_6_clone
+nest_6_dclone 14993/s            --           -3%
+nest_6_clone  15456/s            3%            --
+
+Benchmark: timing 100000 iterations of nest_7_clone, nest_7_dclone...
+nest_7_clone: 7.40539 wallclock secs ( 7.40 usr +  0.00 sys =  7.40 CPU) @ 13513.51/s (n=100000)
+nest_7_dclone: 7.18958 wallclock secs ( 7.18 usr +  0.00 sys =  7.18 CPU) @ 13927.58/s (n=100000)
+                 Rate  nest_7_clone nest_7_dclone
+nest_7_clone  13514/s            --           -3%
+nest_7_dclone 13928/s            3%            --
+
+7重構造で逆転した
